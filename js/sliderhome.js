fd4c13f15f0508f8f443673ae5d12c209f2cd3ca
Luis Fernando <serraluisfernando@gmail.com>
2019-01-18 15:50:04 -0200

    Ajustes no slider home

--- js/sliderhome.js
+++ js/sliderhome.js
@@ -1,25 +1,12 @@
 $(document).ready(function(){
- $('.owl-carousel').owlCarousel({
-    items:1,
+
+	$('.owl-carousel').owlCarousel({
     margin:1,
-    autoHeight: true,
-    nav: false,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 5000,
-    autoplayHoverPause: true,
-    autoplaySpeed: 1800,
- });	
-
- var owl = $('.owl-carousel');
-
- $('.next').click(function() {
-    owl.trigger('next.owl.carousel', [1500]);
+    loop:true,
+    autoWidth:false,
+    items:1,
+    
 })
-// Go to the previous item
-$('.prev').click(function() {
-    // With optional speed parameter
-    // Parameters has to be in square bracket '[]'
-    owl.trigger('prev.owl.carousel', [1500]);
-})
+	
+	
 });
