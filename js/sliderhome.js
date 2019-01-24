f84e08779dcb9e100bef4f07bd2712d5e94c627b
Luis Fernando <serraluisfernando@gmail.com>
2019-01-24 16:57:11 -0200

    Ajuste base no slider

--- js/sliderhome.js
+++ js/sliderhome.js
@@ -5,8 +5,28 @@ $(document).ready(function(){
     loop:true,
     autoWidth:false,
     items:1,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    autoplaySpeed: 1500,
     
 })
+var owl = $('.owl-carousel');
+owl.owlCarousel();
+// Go to the next item
+$('.botnext').click(function() {
+    owl.trigger('next.owl.carousel', [1500]);
+
+    
+})
+// Go to the previous item
+$('.botprev').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [1500]);
+})
+
+
 	
 	
 });
