bb4bebf7fcb1db777f3ea2e0ca19fdb783505491
Luis Fernando <serraluisfernando@gmail.com>
2019-02-19 16:02:05 -0300

    ultima mudanca no html do site

--- js/menu-responsivo.js
+++ js/menu-responsivo.js
@@ -1,6 +1,16 @@
 //Função para o menu responsivo do site
 $(document).ready(function(){
   $('.toggle').click(function(){
-     $('ul').toggleClass('active');
+     $('ul').toggleClass('active');  
+     $('.toggle').toggleClass('active');      
   })
+
+   $('.toggle-static').click(function(){
+     $('.staticplay').toggleClass('active');
+     $('.toggle-static').toggleClass('active');
+  })
+
+
+  
+
 })
